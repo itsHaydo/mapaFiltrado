8f119f05caae10f540548018f8a15e2d05259a80
JackyFC534 <2130173@upv.edu.mx>
2024-03-07 22:37:36 -0600

    Prueba filtrado flask

--- Flask.py
+++ Flask.py
@@ -1,26 +1,80 @@
-
-from flask import Flask, request
+from flask import Flask, render_template, request
 import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
 
-from main import contenido
-
 app = Flask(__name__)
+
+datos = pd.read_csv('CSV/housing.csv', header=None, skiprows=1)
+datos.columns = ['Longitude', 'Latitude', 'Age', 'Rooms', 'Bedrooms', 'Population', 'Households', 'Income', 'Value', 'Ocean Proximity']
+
+def contenido(age, rooms, bedrooms, pop, households, income, value, proximity):
+    texto = f'''<strong>Edad media de la vivienda:</strong> {age}<br>
+                <strong>Total de habitaciones:</strong> {rooms}<br>
+                <strong>Total de dormitorios:</strong> {bedrooms}<br>
+                <strong>Población:</strong> {pop}<br>
+                <strong>Hogares:</strong> {households}<br>
+                <strong>Ingreso mediano:</strong> {income}<br>
+                <strong>Valor mediano de la vivienda:</strong> {value}<br>
+                <strong>Proximidad al océano:</strong> {proximity}'''
+
+    return texto
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/update_map', methods=['POST'])
 def update_map():
-    datos = pd.read_csv('CSV/housing.csv', header=None, skiprows=1)
+    selected_age_filter = request.form.get('age_filter')
+    selected_rooms_filter = request.form.get('rooms_filter')
+    selected_bedrooms_filter = request.form.get('bedrooms_filter')
+    selected_population_filter = request.form.get('population_filter')
+    selected_households_filter = request.form.get('households_filter')
+    selected_median_income_filter = request.form.get('median_income_filter')
+    selected_median_house_value_filter = request.form.get('median_house_value_filter')
+    selected_ocean_filter = request.form.get('ocean_filter')
 
-    selected_filter = request.form['filter']
-    filtered_data = datos  # Aquí debes filtrar los datos según el filtro seleccionado
+    filtered_data = datos
 
-    mapa_actualizado = folium.Map(location=[filtered_data[1].mean(), filtered_data[0].mean()], zoom_start=8)
-    cluster = MarkerCluster().add_to(mapa_actualizado)
+    if selected_age_filter != 'all':
+        age_range = selected_age_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Age'] >= float(age_range[0])) & (filtered_data['Age'] <= float(age_range[1]))]
+
+    if selected_rooms_filter != 'all':
+        rooms_range = selected_rooms_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Rooms'] >= int(rooms_range[0])) & (filtered_data['Rooms'] <= int(rooms_range[1]))]
+
+    if selected_bedrooms_filter != 'all':
+        bedrooms_range = selected_bedrooms_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Bedrooms'] >= int(bedrooms_range[0])) & (filtered_data['Bedrooms'] <= int(bedrooms_range[1]))]
+
+    if selected_population_filter != 'all':
+        population_range = selected_population_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Population'] >= int(population_range[0])) & (filtered_data['Population'] <= int(population_range[1]))]
+
+    if selected_households_filter != 'all':
+        households_range = selected_households_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Households'] >= int(households_range[0])) & (filtered_data['Households'] <= int(households_range[1]))]
+
+    if selected_median_income_filter != 'all':
+        income_range = selected_median_income_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Income'] >= float(income_range[0])) & (filtered_data['Income'] <= float(income_range[1]))]
+
+    if selected_median_house_value_filter != 'all':
+        value_range = selected_median_house_value_filter.split(' - ')
+        filtered_data = filtered_data[(filtered_data['Value'] >= float(value_range[0])) & (filtered_data['Value'] <= float(value_range[1]))]
+
+    if selected_ocean_filter != 'all':
+        filtered_data = filtered_data[filtered_data['Ocean Proximity'] == selected_ocean_filter]
+
+    mapa = folium.Map(location=[filtered_data['Latitude'].mean(), filtered_data['Longitude'].mean()], zoom_start=5)
+    cluster = MarkerCluster().add_to(mapa)
 
     for i, (latitud, longitud, age, rooms, bedrooms, pop, households, income, value, proximity) in enumerate(
-            zip(filtered_data[1], filtered_data[0], filtered_data[2], filtered_data[3], filtered_data[4],
-                filtered_data[5], filtered_data[6], filtered_data[7], filtered_data[8], filtered_data[9])):
+            zip(filtered_data['Latitude'], filtered_data['Longitude'], filtered_data['Age'], filtered_data['Rooms'],
+                filtered_data['Bedrooms'], filtered_data['Population'], filtered_data['Households'],
+                filtered_data['Income'], filtered_data['Value'], filtered_data['Ocean Proximity'])):
 
         popup = contenido(age, rooms, bedrooms, pop, households, income, value, proximity)
 
@@ -30,7 +84,8 @@ def update_map():
             icon=folium.Icon(color='blue')
         ).add_to(cluster)
 
-    return mapa_actualizado.get_root().html.render()
+    return mapa._repr_html_()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
